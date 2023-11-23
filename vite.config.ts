07485c9030cb9d660aa89833122a8e41023b8fef
Alon Pinto <pintoalon@gmail.com>
2023-11-23 15:06:28 +0200

    config pwa application

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,76 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import mkcert from "vite-plugin-mkcert";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: { https: true }, // Not needed for Vite 5+
-  plugins: [react(), mkcert()],
+  plugins: [
+    react(),
+    mkcert(),
+    VitePWA({
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      manifest: {
+        name: "AppOnTime",
+        short_name: "AppOnTime",
+        description: "My Awesome App description",
+        theme_color: "#ffffff",
+        background_color: "#fafafa",
+        display: "standalone",
+        scope: "./",
+        start_url: "./",
+        icons: [
+          {
+            src: "assets/icons/icon-72x72.png",
+            sizes: "72x72",
+            type: "image/png",
+            purpose: "maskable any",
+          },
+          {
+            src: "assets/icons/icon-96x96.png",
+            sizes: "96x96",
+            type: "image/png",
+            purpose: "maskable any",
+          },
+          {
+            src: "assets/icons/icon-128x128.png",
+            sizes: "128x128",
+            type: "image/png",
+            purpose: "maskable any",
+          },
+          {
+            src: "assets/icons/icon-144x144.png",
+            sizes: "144x144",
+            type: "image/png",
+            purpose: "maskable any",
+          },
+          {
+            src: "assets/icons/icon-152x152.png",
+            sizes: "152x152",
+            type: "image/png",
+            purpose: "maskable any",
+          },
+          {
+            src: "assets/icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable any",
+          },
+          {
+            src: "assets/icons/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+            purpose: "maskable any",
+          },
+          {
+            src: "assets/icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable any",
+          },
+        ],
+      },
+    }),
+  ],
   base: "/site",
 });
