f31673d99ec35aeba6b6bebf36c0ac445df998b2
Alon Pinto <pintoalon@gmail.com>
2023-11-23 15:23:15 +0200

    move config to manifest to file

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import react from "@vitejs/plugin-react-swc";
+import * as path from "path";
 import { defineConfig } from "vite";
 import mkcert from "vite-plugin-mkcert";
 import { VitePWA } from "vite-plugin-pwa";
+import manifest from "./manifest.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,67 +12,13 @@ export default defineConfig({
     mkcert(),
     VitePWA({
       includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
-      manifest: {
-        name: "AppOnTime",
-        short_name: "AppOnTime",
-        description: "My Awesome App description",
-        theme_color: "#ffffff",
-        background_color: "#fafafa",
-        display: "standalone",
-        scope: "./",
-        start_url: "./",
-        icons: [
-          {
-            src: "assets/icons/icon-72x72.png",
-            sizes: "72x72",
-            type: "image/png",
-            purpose: "maskable any",
-          },
-          {
-            src: "assets/icons/icon-96x96.png",
-            sizes: "96x96",
-            type: "image/png",
-            purpose: "maskable any",
-          },
-          {
-            src: "assets/icons/icon-128x128.png",
-            sizes: "128x128",
-            type: "image/png",
-            purpose: "maskable any",
-          },
-          {
-            src: "assets/icons/icon-144x144.png",
-            sizes: "144x144",
-            type: "image/png",
-            purpose: "maskable any",
-          },
-          {
-            src: "assets/icons/icon-152x152.png",
-            sizes: "152x152",
-            type: "image/png",
-            purpose: "maskable any",
-          },
-          {
-            src: "assets/icons/icon-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-            purpose: "maskable any",
-          },
-          {
-            src: "assets/icons/icon-384x384.png",
-            sizes: "384x384",
-            type: "image/png",
-            purpose: "maskable any",
-          },
-          {
-            src: "assets/icons/icon-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "maskable any",
-          },
-        ],
-      },
+      manifest,
     }),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   base: "/site",
 });
