1f786abe56fd1d302d0c60911d9bc8d89047e8c0
Alon Pinto <pintoalon@gmail.com>
2023-11-23 12:35:44 +0200

    add https to dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: { https: true }, // Not needed for Vite 5+
+  plugins: [react(), mkcert()],
   base: "/site",
 });
